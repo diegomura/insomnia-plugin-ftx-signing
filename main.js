7a5674ab911b63e1d042ef7d3abd3fdb930d06aa
diegomura <diegomuracciole@gmail.com>
2021-05-10 14:09:41 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const CryptoJS = require('crypto-js')
+
+const ftxUrl = 'https://ftx.com/api'
+
+function computeHttpSignature(msg, key) {
+  const hash = CryptoJS.HmacSHA256(msg, key)
+  return CryptoJS.enc.Hex.stringify(hash)
+}
+
+// A request hook will be run before sending the request to API, but after everything else is finalized
+module.exports.requestHooks = [
+  (context) => {
+    const req = context.request
+    // Validate URL
+    if (
+      !req.hasOwnProperty('getUrl') ||
+      req['getUrl'] == null ||
+      req['getUrl'].constructor.name != 'Function' ||
+      !req.getUrl().startsWith(ftxUrl)
+    ) {
+      console.log('Not a FTX API URL')
+      return
+    }
+
+    // Check for a valid api key
+    const key = req.getEnvironmentVariable('API_SECRET')
+    if (key == null) {
+      console.log('Could not find environment variable "API_SECRET". Cannot sign message')
+      throw new Error("Message should be signed, but cannot find 'API_SECRET' environment variable.")
+    }
+
+    const method = req.getMethod();
+    const timestamp = req.getHeader('FTX-TS')
+    const path = (new URL(req.getUrl())).pathname
+    const body = req.getBody().text || ''
+
+
+    const message = `${timestamp}${method}${path}${body}`
+    const signature = computeHttpSignature(message, key)
+
+    req.setHeader('FTX-SIGN', signature)
+  },
+]
